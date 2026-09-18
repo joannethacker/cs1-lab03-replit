2b940e61c50c2984e4a592bb7e94a909865dccbc
joannethacker <jo.taormina@gmail.com>
2026-09-18 17:54:31 -0400

    Create Lab3App.java

---
+++ Lab3App.java
@@ -0,0 +1,33 @@
+// The import statement allows program to use the cardgames.jar file
+import cardgames.*;
+
+/**
+ * <p>Title: </p>
+ * 
+ * <p>Description: </p>
+ * 
+ * @author your names here
+ */
+
+public class Lab3App {
+
+	public static void main(String[] args)
+	{
+		//
+    GUI theWindow = new Deck();
+		Card theDeck = new Deck();
+		Card card1;
+
+    //
+		theDeck.shuffleDeck();
+
+    //
+		card1 = theWindow.dealCard();
+
+    //
+		theWindow.showCard(card1);
+
+    //
+		System.out.println(card1.toString());	
+	}
+}
