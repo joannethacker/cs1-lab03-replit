716bffe3355451743152976e8420a4c29ff3b8b0
joannethacker <jo.taormina@gmail.com>
2026-09-18 22:50:09 -0400

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,6 +1,14 @@
 { pkgs }: {
   deps = [
-    pkgs.graalvm-ce
+    pkgs.psmisc
+    pkgs.jdk17
     pkgs.jdt-language-server
+    pkgs.fontconfig
+    pkgs.dejavu_fonts
   ];
+
+  env = {
+    XDG_DATA_DIRS = "${pkgs.dejavu_fonts}/share";
+    FONTCONFIG_FILE = "${pkgs.fontconfig.out}/etc/fonts/fonts.conf";
+  };
 }
